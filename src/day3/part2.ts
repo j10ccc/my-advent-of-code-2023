0fcfdb241f9f179ac18aa3ab1ce1dd326a742319
j10c <blyb1739@gmail.com>
2024-02-02 20:13:37 +0800

    feat(day3): part2 solution

--- src/day3/part2.ts
+++ src/day3/part2.ts
@@ -1,57 +1,15 @@
-export function getNumber(index: number, context: string): [value: number, pos: number] {
-  let left = index;
-  let right = index;
-  let pos = index;
-  let str = context[index];
-
-  while(left > 0) {
-    left--;
-    if (isDigit(context[left])) {
-      str = context[left] + str;
-    } else {
-      pos = left + 1;
-      break;
-    }
-  }
-
-  while(right < context.length - 1) {
-    right++;
-    if (isDigit(context[right])) {
-      str = str + context[right];
-    } else {
-      break;
-    }
-  }
-
-  return [parseInt(str), pos];
-}
-
-export function isDigit(char: string) {
-  if (char <= "9" && char >= "0") return true;
-  // if (char === "-") return true;
-
-  return false;
-}
-
-export function isSymbol(char: string) {
-  if (char <= "9" && char >= "0") return false;
-  if (char === ".") return false;
-  if (char === "\n") return false;
-  // if (char === "-") return false;
-
-  return true;
-}
+import { isSymbol, getNumber, isDigit } from "./part1";
 
 export default function day3_part1(input: string) {
   const lines = input.split("\n");
   const context = input;
   const width = lines[0].length + 1;
   let sum = 0;
-  const map = new Map<number, number>();
 
   for (let i = 0; i < context.length; i++) {
     const char = context[i];
     if (isSymbol(char)) {
+      const map = new Map<number, number>();
       if (isDigit(context[i - width])) {
         const [value, id] = getNumber(i - width, context);
         map.set(id, value);
@@ -88,11 +46,15 @@ export default function day3_part1(input: string) {
           map.set(id, value);
         }
       }
-    }
-  }
 
-  for (const value of map.values()) {
-    sum += value;
+      if (map.size === 2) {
+        let tmp = 1;
+        for (const value of map.values()) {
+          tmp *= value;
+        }
+        sum += tmp;
+      }
+    }
   }
 
   return sum;
